974b527f23b9e85b48652a49a8628e9cfb91d0fd
eduardo <eduardo@DESKTOP-V7VSV56>
2017-06-03 17:59:15 -0300

    ADD Entitys classes

---
+++ dellas/src/main/java/com/dellas/model/Pedido.java
@@ -0,0 +1,213 @@
+package com.dellas.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import br.com.natural.entidade.Marcacao;
+
+@Entity
+@Table(name="PEDIDO")
+public class Pedido implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "DESCRICAO", nullable = false, length = 200)
+	private String descricao;
+	
+	@Column(name = "DATA")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data;
+		
+	@Column(name = "VALOR_TOTAL_ITENS")
+	private double valorTotalItens;
+	
+	@Column(name = "VALOR_DESCONTO")
+	private double valorDesconto;
+	
+	@Column(name = "FUNCIONARIO")
+	private Usuario funcionario;
+	
+	@Column(name = "STATUS")
+	private String status;
+	
+	@Column(name="PRODUTOS")
+	@OneToMany(mappedBy="PRODUTOS")
+	private List<Produto> produtos;
+
+	@Column(name = "OBSERVACAO" , length = 200)
+	private String observacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public double getValorTotalItens() {
+		return valorTotalItens;
+	}
+
+	public void setValorTotalItens(double valorTotalItens) {
+		this.valorTotalItens = valorTotalItens;
+	}
+
+	public double getValorDesconto() {
+		return valorDesconto;
+	}
+
+	public void setValorDesconto(double valorDesconto) {
+		this.valorDesconto = valorDesconto;
+	}
+
+	public Usuario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Usuario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((funcionario == null) ? 0 : funcionario.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((observacao == null) ? 0 : observacao.hashCode());
+		result = prime * result + ((produtos == null) ? 0 : produtos.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(valorDesconto);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(valorTotalItens);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (funcionario == null) {
+			if (other.funcionario != null)
+				return false;
+		} else if (!funcionario.equals(other.funcionario))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (observacao == null) {
+			if (other.observacao != null)
+				return false;
+		} else if (!observacao.equals(other.observacao))
+			return false;
+		if (produtos == null) {
+			if (other.produtos != null)
+				return false;
+		} else if (!produtos.equals(other.produtos))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (Double.doubleToLongBits(valorDesconto) != Double.doubleToLongBits(other.valorDesconto))
+			return false;
+		if (Double.doubleToLongBits(valorTotalItens) != Double.doubleToLongBits(other.valorTotalItens))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", descricao=" + descricao + ", data=" + data + ", valorTotalItens="
+				+ valorTotalItens + ", valorDesconto=" + valorDesconto + ", funcionario=" + funcionario + ", status="
+				+ status + ", produtos=" + produtos + ", observacao=" + observacao + "]";
+	}
+
+	
+	
+	
+	
+}
+
---
+++ dellas/src/main/java/com/dellas/model/Usuario.java
@@ -0,0 +1,116 @@
+package com.dellas.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USUARIO")
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+
+	@Column(name = "USR_NOME", nullable = false, length = 100)
+	private String nome;
+	
+	@Column(name = "USR_PASSWORD", nullable = false, length = 50)
+	private String password;
+
+	@Column(name = "URS_TELEFONE", nullable = false, length = 50)
+	private String telefone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", password=" + password + ", telefone=" + telefone + "]";
+	}
+	
+	
+}
