627bd1e363e4bb96929742be63e4c29a586fcbad
eduardobaia <eduardo_baia@msn.com>
2017-06-03 14:35:03 -0300

    inicial commit

---
+++ dellas/src/main/java/com/dellas/dellas/DellasApplication.java
@@ -0,0 +1,12 @@
+package com.dellas.dellas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DellasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DellasApplication.class, args);
+	}
+}
