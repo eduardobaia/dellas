bb2b2bef030899f0f31edaf3847f73e3ec7b0a70
jose.eduardo.baia <jose.eduardo.baia@CPX-O6ESUXNZ6ZN.dir.svc.accenture.com>
2017-06-05 15:41:46 -0300

    Ajustments

--- dellas/src/main/java/com/dellas/controller/PedidoController.java
+++ dellas/src/main/java/com/dellas/controller/PedidoController.java
@@ -6,17 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dellas.dto.PedidoDTO;
 import com.dellas.exception.PedidoExceptionHandler;
 import com.dellas.service.PedidoService;
 import com.dellas.exception.Error;
 
+@RequestMapping("/pedido")
+@CrossOrigin
+@RestController
 public class PedidoController {
 
 
